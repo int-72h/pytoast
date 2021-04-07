bcd2aa1090c1dc7247284b316d10802918efb609
int-72h <int-10h@protonmail.com>
2021-04-08 00:18:39 +0100

    Tidied up SQL file finding code, formatted as a pip package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+setup(
+    name='ofatomic',
+    version='0.0.1',
+    description='A command line launcher for Open Fortress',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/int-72h/ofatomic',
+    author='int',
+    packages=['ofatomic'],
+    python_requires='>=3.4',
+    package_data={ 
+        'ofatomic': ['public.pem'],
+    },
+    entry_points={ 
+        'console_scripts': [
+            'ofatomic=ofatomic:main',
+        ],
+    },
+)
