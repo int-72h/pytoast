12d5c2afa671113646c73dd5e28c738a24f8d3c3
int-72h <int-10h@protonmail.com>
2021-05-06 00:33:35 +0100

    fix to packaging #2

--- setup.py
+++ setup.py
@@ -4,18 +4,16 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 setup(
     name='ofatomic',
-    version='210506.0',
+    version='210506.1',
     description='A command line launcher for Open Fortress',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/int-72h/ofatomic',
     zip_safe=False,
     author='int',
+    include_package_data=True,
     packages=['ofatomic'],
     python_requires='>=3.4',
-    package_data={
-        'ofatomic': ['ofatomic/ofpublic.pem', 'ofatomic/gameinfo.txt'],
-    },
     entry_points={ 
         'console_scripts': [
             'ofatomic=ofatomic:main',
